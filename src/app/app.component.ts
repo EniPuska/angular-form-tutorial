12ee8c65b7bb19bb3c061a0bcb612a7f015d895a
EniPuska <eni.puska@gmail.com>
2020-11-17 18:44:10 +0200

    ...

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,12 +8,35 @@ import { NgForm } from '@angular/forms';
 })
 export class AppComponent {
   @ViewChild('f') signUpForm: NgForm; 
+  defaultQuestion = 'teacher';
   answer: string = '';
+  genders: Array<string> = ['male', 'female'];
+  user = {
+    username: '',
+    email: '',
+    secret: '',
+    questionAnswer: '',
+    gender: ''
+  }
+  submitted: boolean = false;
   suggestUserName() {
     const suggestedName = 'Superuser';
+    this.signUpForm.form.patchValue({
+      userData: {
+        username: suggestedName
+      }
+    });
   }
 
   onSubmit() {
-    console.log(this.signUpForm); 
+    this.user.username = this.signUpForm.value.userData.username;
+    this.user.email = this.signUpForm.value.userData.email;
+    this.user.secret = this.signUpForm.value.secret;
+    this.user.questionAnswer = this.signUpForm.value.questionAnswer;
+    this.user.gender = this.signUpForm.value.gender;
+    this.submitted = true;
+
+    this.signUpForm.reset();
+    
   }
 } 
